8ab19b802199f9a864c93c6153112f1108e917a4
Habib Muzakki Piliang <160248449+habibmuzakkipiliang@users.noreply.github.com>
2025-09-21 08:05:38 +0700

    Add files via upload

---
+++ Latihan_JavaScript_Console_5.js
@@ -0,0 +1,39 @@
+console.log ("Hello World")
+
+let nama = "Hujan Badai";
+
+console.log (nama)
+
+let a = 10;
+let b = 2;
+
+console.log ("Tambah = ", a + b);
+console.log ("Kurang =", a - b);
+console.log ("Kali = ", a * b);
+console.log ("Bagi = ", a / b);
+console.log ("Modulus = ", a % b);
+
+// logika
+
+console.log (a > b); 
+console.log (a < b);
+console.log (a >= b);
+console.log (a <= b);
+console.log (a == b);
+console.log (a != b);
+
+// tipe data
+
+let nama_depan = "Hujan"; 
+let nama_belakang = "Badai";
+let umur = 20;
+let tinggi = 170.5;
+let menikah = false;
+let pekerjaan;
+
+console.log (nama_depan);
+console.log (nama_belakang);
+console.log (umur);
+console.log (tinggi);
+console.log (menikah);
+console.log (pekerjaan);
