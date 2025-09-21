4741393df5fad35b6774b71a4fe7fe48b29c69c7
Habib Muzakki Piliang <160248449+habibmuzakkipiliang@users.noreply.github.com>
2025-09-21 08:18:31 +0700

    Add files via upload

---
+++ Latihan_JavaScript_Console_2.js
@@ -0,0 +1,64 @@
+console.log ("Hallo Dunia")
+
+// sintaks dasar JavaScript
+
+let nama = "Selamat Belajar JavaScript"
+console.log (nama)
+
+// tipe data
+
+let teks = "Hujan Badai"
+let angka = 20
+let desimal = 10.5
+let benar = true
+let salah = false
+let kosong
+
+console.log (teks)
+console.log (nama)
+console.log (angka)
+console.log (desimal)
+console.log (benar)
+console.log (salah)
+console.log (kosong)
+
+let a = 10
+let b = 20
+
+// operasi aritmatika
+
+console.log (a + b)
+console.log (a * b)
+console.log (a / b)
+console.log (a - b)
+console.log (a % b)
+console.log (a ** b)
+
+// logika
+
+console.log (a > b)
+console.log (a < b)
+console.log (a >= b)
+console.log (a <= b)
+console.log (a == b)
+console.log (a != b)
+
+// percabangan
+
+let x = 10
+
+if (x == 20) {
+    console.log ("x sama dengan 20")
+}
+
+else if (x == 30) {
+    console.log ("x sama dengan 30")
+}
+
+else if (x == 10) {
+    console.log ("x sama dengan 10")
+}
+
+else {
+    console.log ("x tidak sama dengan 10, 20, atau 30")
+}
