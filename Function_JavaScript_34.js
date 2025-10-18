83a2d4450b2113c520ea03c7e46459fe30748523
Habib Muzakki Piliang <160248449+habibmuzakkipiliang@users.noreply.github.com>
2025-10-18 12:17:05 +0700

    Add files via upload

---
+++ Function_JavaScript_34.js
@@ -0,0 +1,59 @@
+function haloSiswa (nama) {
+    console.log ("Halo " + nama)
+}
+
+haloSiswa ("Habib")
+haloSiswa ("Hayyan")
+haloSiswa ("Fachri")
+
+
+function defUltra () {
+    console.log ("Hello World")
+    console.log ("Hello Helium")
+    console.log ("Hello Lithium")
+}
+
+defUltra ()
+defUltra ()
+
+function defMega () {
+    console.log ("Test Mega Function")
+}
+
+defMega ()
+
+function haloSapa (nama) {
+    console.log ("Halo " + nama)
+}
+
+haloSapa ("Alice")
+haloSapa ("Bob")
+haloSapa ("Charlie")
+
+function haloGood (namaSapa) {
+    console.log ("Halo " + namaSapa + ", Semoga Harimu Menyenangkan!")
+}
+
+haloGood ("Diana")
+haloGood ("Ethan")
+
+function tambah (a, b) {
+    return a + b
+}
+
+hasil = tambah (10, 15)
+console.log (hasil)
+
+function kali (x, y) {
+    return x * y
+}
+
+hasil = kali (9, 10) 
+console.log (hasil)
+
+function bagi (m, n) {
+    return m / n
+}
+
+hasil = bagi (20, 4)
+console.log (hasil)
