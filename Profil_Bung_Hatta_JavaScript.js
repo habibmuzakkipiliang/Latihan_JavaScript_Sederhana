59d6c0c0f368860364c5c1a03208382e37529c6d
Habib Muzakki Piliang <160248449+habibmuzakkipiliang@users.noreply.github.com>
2025-10-18 12:15:47 +0700

    Add files via upload

---
+++ Profil_Bung_Hatta_JavaScript.js
@@ -0,0 +1,26 @@
+let namaAsli = "Muhammad Athar"
+let akrab = "Bung Hatta"
+let lahir = "19 Agustus 1902 "
+let tempatLahir = "Kota Bukittinggi"
+let asalProvinsi = "Sumatra Barat"
+let makam = "Taman Pemakaman Umum Tanah Kusir"
+let tanggalWafat = "14 Maret 1980"
+let tempatWafat = "Jakarta"
+let usia = "77 tahun"
+let pekerjaan = "Politikus, Ekonom"
+let etnis = "Minangkabau"
+let klan = "Piliang"
+
+
+console.log ("Nama asli :", namaAsli)
+console.log ("Akrab :", akrab)
+console.log ("Lahir :", lahir)
+console.log ("Tempat lahir :", tempatLahir)
+console.log ("Asal Provinsi :", asalProvinsi)
+console.log ("Makam :", makam)
+console.log ("Tanggal wafat :", tanggalWafat)
+console.log ("Usia :", usia)
+console.log ("Tempat Wafat :", tempatWafat)
+console.log ("Pekerjaan :", pekerjaan)
+console.log ("Etnis :", etnis)
+console.log ("Klan :", klan)
