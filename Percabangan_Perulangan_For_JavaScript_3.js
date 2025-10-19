abfa082275837a7cfff89c9efd054a0c9788356a
Habib Muzakki Piliang <160248449+habibmuzakkipiliang@users.noreply.github.com>
2025-10-19 15:56:13 +0700

    Add files via upload

---
+++ Percabangan_Perulangan_For_JavaScript_3.js
@@ -0,0 +1,175 @@
+// percabangan dasar
+
+
+let u = 7
+
+if (u > 5) {
+    console.log ("Besar")
+}
+
+else {
+    console.log ("Sama")
+}
+
+// percabangan lanjutan 1
+
+let w = 6
+
+if (w > 5) {
+    if (true) {
+        console.log ("Besar")
+    }
+    
+    else if (w == 4) {
+        console.log ("4")
+    }
+    
+    else if (w == 3) {
+        console.log ("3")
+    }
+    
+    else if (w == 2) {
+        console.log ("2")
+    }
+    
+    else if (w == 1) {
+        console.log ("1")
+    }
+}
+
+else {
+    console.log ("default")
+}
+
+// percabangan lanjutan 2
+
+let k = 5
+
+
+if (k > 7) {
+    console.log ("3")
+}
+
+else if (k == 3) {
+    console.log ("3")
+}
+
+else if (k == 2) {
+    console.log ("2")
+}
+
+else if (k == 1) {
+    console.log ("1")
+}
+
+else {
+    console.log ("sama saja")
+}
+
+// Perulangan for
+
+for (e = 0; e < 6; e++) {
+    console.log ("e", e)
+}
+
+for (x = 0; x < 6; x++) {
+    console.log ("x", x)
+}
+
+
+for (f = 0; f < 6; f++) {
+    console.log ("f", f)
+}
+
+for (v = 0; v < 6; v++) {
+    console.log ("v", v)
+}
+
+// perulangan while
+
+let l = 0
+
+while (l < 6) {
+    console.log ("l", l)
+    l++
+}
+
+let jet = 0
+
+while (jet < 6) {
+    console.log ("jet", jet)
+    jet++
+}
+
+let h = 0
+
+while (h < 6) {
+    console.log ("h", h)
+    h++
+}
+
+// do while
+
+let wet = 0
+
+do {
+    console.log ("wet", wet)
+    wet++
+} while (wet < 6)
+
+
+let er = 0
+
+do {
+    console.log ("er", er)
+    er++
+} while (er < 6)
+
+let qw = 0
+
+do {
+    console.log ("qw", qw)
+    qw++
+} while (qw < 6)
+
+
+// switch case
+
+let nilai = 5
+
+switch (nilai) {
+    case 1:
+        console.log ("1")
+        break
+        
+    case 2:
+        console.log ("2")
+        break
+        
+    case 3:
+        console.log ("3")
+        break
+        
+    case 4:
+        console.log ("4")
+        break
+        
+    case 5:
+        console.log ("5")
+        break
+        
+    case 6:
+        console.log ("6")
+        break
+        
+    case 7:
+        console.log ("7")
+        break
+        
+    case 8:
+        console.log ("8")
+        break
+        
+    default:
+    console.log ("default")
+}
