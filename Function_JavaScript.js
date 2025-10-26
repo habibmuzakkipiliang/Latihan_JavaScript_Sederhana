fae4fda8e5d526e23567a2ee3ab484f6318fdfb7
Habib Muzakki Piliang <160248449+habibmuzakkipiliang@users.noreply.github.com>
2025-10-26 08:23:58 +0700

    Add files via upload

---
+++ Function_JavaScript.js
@@ -0,0 +1,95 @@
+function tam () {
+    console.log ("Halo")
+}
+
+tam ()
+tam ()
+tam ()
+
+
+function ter (ft) {
+    console.log ("halo " + ft + ", semoga hari menyenangkan" + "dan semakin baik performanya")
+}
+
+ter ("vfeliciwa")
+ter ("yovita")
+ter ("theresa")
+
+function er (nat) {
+    console.log ("Halo " + nat + ", Semoga hari kamu semakin baik")
+}
+
+er ("vfeliciwa")
+er ("yovita_15")
+
+function wrt (er) {
+    console.log ("Halo " + er + ", Cantik")
+}
+
+wrt ("vfeliciwa")
+wrt ("yovita_15")
+wrt ("kezia")
+
+function tem () {
+    console.log ("Hai")
+    console.log ("Hi")
+    console.log ("Tas")
+    console.log ("dik")
+}
+
+tem ()
+tem ()
+tem ()
+
+function wer () {
+    console.log ("Loco")
+    console.log ("Spanyol")
+    console.log ("Portugis")
+    console.log ("Espanol")
+}
+
+wer ()
+wer ()
+wer ()
+
+function ret (a, b) {
+    return a + b
+}
+
+hasil = ret (5, 4)
+console.log (hasil)
+
+function rat (r, t) {
+    return r ** t
+}
+
+hasil = rat (4, 5)
+console.log (hasil)
+
+function tambah (a, b) {
+    return a + b
+}
+
+hasil = tambah (10, 15)
+console.log (hasil)
+
+function kali (x, y) {
+    return x * y
+}
+
+hasil = kali (9, 10) 
+console.log (hasil)
+
+function bagi (m, n) {
+    return m / n
+}
+
+hasil = bagi (20, 4)
+console.log (hasil)
+
+function pangkat (e, r) {
+    return e ** r
+}
+
+hasil = pangkat (5, 2)
+console.log (hasil)
